406d1cdc98ab39fe09d7ec124f6e46c47fb4a8f5
Austin <abharms@gmail.com>
2016-07-17 18:35:35 -0500

    add about us page

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,13 @@ var headerContactInfo = $('.header-contact-info');
 $(window).resize(function() {
 	if ($(window).width() < 992) {
 			$(menu).addClass('nav-stacked');
-			$(headerContactInfo).hide();
+			
 	} else {
 		$(menu).removeClass('nav-stacked');
-		$(headerContactInfo).show();
+
 }
 
-}).resize();
+});
 
 jQuery(document).ready(function($) {
 			$('.my-slider').unslider();
